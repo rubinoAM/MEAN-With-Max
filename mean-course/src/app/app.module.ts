4ae06d0f42516067082ac7b5b7c6efb225b36341
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-10 15:46:40 -0400

    Added navbar:

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
     FormsModule,
     BrowserAnimationsModule
   ],
