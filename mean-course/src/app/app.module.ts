0b586493596b7a2bfdc341c5c405484a335608ec
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-10 15:42:26 -0400

    Added styling

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
     FormsModule,
     BrowserAnimationsModule
   ],
--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     selector:'app-post-create',
     templateUrl:'./post-create.component.html',
+    styleUrls: ['./post-create.component.css']
 })
 
 export class PostCreateComponent{
-    post:string="";
+    post:string="Type in some text above and save your post to overwrite this text.";
     enteredPost:string="";
 
     savePost(){
