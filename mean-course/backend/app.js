2a806be6a5f8911571c2ed9731079c589ec9b73a
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-16 11:17:28 -0400

    Angular can access POST request to api/posts

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -19,12 +19,12 @@ app.use((req,res,next)=>{
 app.post('/api/posts',(req,res,next)=>{
     const post = req.body;
     console.log(post);
-    res.status(201),json({
+    res.status(201).json({
         message: 'Post added',
     });
 });
 
-app.use('/api/posts',(req,res,next)=>{
+app.get('/api/posts',(req,res,next)=>{
     const posts = [
         {
             id:'f48ha9sf',
