a55eabd158caa78885083e06089a8235248490c9
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-04 22:03:59 -0400

    Signup Process

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 
 import { AuthData } from '../models/auth-data.model';
 
 @Injectable({ providedIn:'root' })
 export class AuthService{
-    constructor(private http:HttpClient, private router:Router){}
+    constructor(private http:HttpClient){}
 
     createUser(email:string,password:string){
         const authData:AuthData = {email:email,password:password};
         this.http.post("http://localhost:4201/api/auth/signup",authData)
+            .subscribe(resp => {
+                console.log(resp)
+            })
+    }
+
+    login(email:string,password:string){
+        const authData:AuthData = {email:email,password:password};
+        this.http.post("http://localhost:4201/api/auth/login",authData)
             .subscribe(resp => {
                 console.log(resp)
             })
