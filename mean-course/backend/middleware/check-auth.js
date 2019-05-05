d9f387fd6a392189c076f632eff225d12ff577ad
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-04 22:23:19 -0400

    Protecting routes

--- mean-course/backend/middleware/check-auth.js
+++ mean-course/backend/middleware/check-auth.js
@@ -5,6 +5,7 @@ module.exports = (req,res,next)=>{
     try{
         const token = req.headers.authorization.split(" ")[1];
         jwt.verify(token,config.secretHash);  
+        next();
     } catch(err){
         res.status(401).json({
             message:'Authorization failed'
--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
 
 const Post = require('../models/post');
 
@@ -27,7 +28,7 @@ const storage = multer.diskStorage({
     }
 });
 
-router.post('',multer({storage:storage}).single("image"),(req,res,next)=>{
+router.post('',checkAuth,multer({storage:storage}).single("image"),(req,res,next)=>{
     const url = req.protocol + '://' + req.get('host');
     const post = new Post({
         title:req.body.title,
@@ -80,7 +81,7 @@ router.get('/:id',(req,res,next)=>{
         });
 })
 
-router.put('/:id',multer({storage:storage}).single("image"),(req,res,next)=>{
+router.put('/:id',checkAuth,multer({storage:storage}).single("image"),(req,res,next)=>{
     let imagePath = req.body.imagePath;
     if(req.file){
         const url = req.protocol + '://' + req.get('host');
@@ -100,7 +101,7 @@ router.put('/:id',multer({storage:storage}).single("image"),(req,res,next)=>{
         });
 })
 
-router.delete("/:id",(req,res,next)=>{
+router.delete("/:id",checkAuth,(req,res,next)=>{
     Post.deleteOne({
         _id:req.params.id,
     }).then((result)=>{
