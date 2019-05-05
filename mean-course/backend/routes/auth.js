c7fa80519c1d4ddc5ca594412ce84255cbcf644f
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-04 22:07:30 -0400

    Login successful

--- mean-course/backend/routes/auth.js
+++ mean-course/backend/routes/auth.js
@@ -29,6 +29,7 @@ router.post('/signup',(req,res,next)=>{
 })
 
 router.post('/login',(req,res,next)=>{
+    let fetchedUser;
     User.findOne({email:req.body.email})
         .then(user =>{
             if(!user){
@@ -36,6 +37,7 @@ router.post('/login',(req,res,next)=>{
                     message:'Login failed',
                 });
             }
+            fetchedUser = user;
             return bcrypt.compare(req.body.password, user.password);
         })
         .then(result =>{
@@ -46,8 +48,8 @@ router.post('/login',(req,res,next)=>{
             }
             const token = jwt.sign(
                 {
-                    email:user.email,
-                    userId:user._id
+                    email:fetchedUser.email,
+                    userId:fetchedUser._id
                 },
                 'hash_secret_166552afbf222226e68edea13ce6121a',
                 {expiresIn: '1h'}
