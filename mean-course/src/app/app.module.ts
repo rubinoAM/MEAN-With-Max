e5b3b0acde73cf3270346f549fdeb3ae10eb6423
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-06 16:44:22 -0400

    Error msg with MatDialog

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { ErrorInterceptor } from './error-interceptor';
     HeaderComponent,
     PostListComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
@@ -64,6 +66,7 @@ import { ErrorInterceptor } from './error-interceptor';
       multi:true,
     }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents:[ErrorComponent]
 })
 export class AppModule { }
