fea2dbb78b261e312379956033167fba1491fc5d
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-12 21:39:42 -0400

    Error msg handling

--- mean-course/backend/routes/auth.js
+++ mean-course/backend/routes/auth.js
@@ -64,7 +64,7 @@ router.post('/login',(req,res,next)=>{
         })
         .catch((err)=> {
             res.status(500).json({
-                error:err
+                message:"Invalid authentication credentials",
             })
         });
 });
