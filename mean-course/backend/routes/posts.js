155766301db569a88cf302a7f5d44001e99b034b
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-12 22:15:25 -0400

    Separating middleware

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -1,36 +1,34 @@
 const express = require('express');
-const multer = require('multer');
 const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/extract-file');
 const postController = require('../controllers/posts');
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-    'image/png':'png',
-    'image/jpg':'jpg',
-    'image/jpeg':'jpg',
-}
-const storage = multer.diskStorage({
-    destination: (req,file,cb)=>{
-        const isValid = MIME_TYPE_MAP[file.mimetype]
-        let error = new Error("Invalid MIME type");
-        if(isValid){
-            error = null
-        }
-        cb(error, "backend/images");
-    },
-    filename:(req,file,cb)=>{
-        const name = file.originalname.toLowerCase().split(' ').join('-');
-        const ext = MIME_TYPE_MAP[file.mimetype];
-        const fileName = `${name}-${Date.now()}.${ext}`;
-        cb(null,fileName)
-    }
-});
-
-router.post('',checkAuth,multer({storage:storage}).single("image"),postController.createPost);
-router.get('',postController.getAllPosts);
-router.get('/:id',postController.getSinglePost);
-router.put('/:id',checkAuth,multer({storage:storage}).single("image"),postController.updatePost)
-router.delete("/:id",checkAuth,postController.deletePost);
+router.post(
+    '',
+    checkAuth,
+    extractFile,
+    postController.createPost
+);
+router.get(
+    '',
+    postController.getAllPosts
+);
+router.get(
+    '/:id',
+    postController.getSinglePost
+);
+router.put(
+    '/:id',
+    checkAuth,
+    extractFile,
+    postController.updatePost
+);
+router.delete(
+    "/:id",
+    checkAuth,
+    postController.deletePost
+);
 
 module.exports = router;
